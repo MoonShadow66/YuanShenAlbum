72d48ba2a6df0c129eb7f75c77097b9f6d67552a
pengliang <3495268188@qq.com>
2022-08-11 11:57:01 +0800

    增加viewpager切换动画

--- app/src/main/java/com/liang/yuanshenalbum/widget/VerticalViewPager.kt
+++ app/src/main/java/com/liang/yuanshenalbum/widget/VerticalViewPager.kt
@@ -10,7 +10,7 @@ class VerticalViewPager(context: Context, attrs: AttributeSet) : ViewPager(conte
 
     init {
         //设置viewpage的切换动画,这里设置才能真正实现垂直滑动的viewpager
-        setPageTransformer(true, DefaultTransformer())
+        setPageTransformer(true, VerticalPageTransformer())
     }
 
     /**
@@ -65,6 +65,43 @@ class VerticalViewPager(context: Context, attrs: AttributeSet) : ViewPager(conte
             }
         }
 
+    }
+
+    /**
+     * 自定义 ViewPager 切换动画
+     * 如果不设置切换动画，还会是水平方向的动画
+     */
+    class VerticalPageTransformer : PageTransformer {
+        private val MIN_SCALE = 0.75f
+        override fun transformPage(view: View, position: Float) {
+            val pageWidth = view.width
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.alpha = 0f
+            } else if (position <= 0) { // [-1,0]
+                // Use the default slide transition when moving to the left page
+                view.alpha = 1f
+                //view.setTranslationX(1);
+                view.scaleX = 1f
+                view.scaleY = 1f
+                val yPosition = position * view.height
+                view.translationY = yPosition
+                view.translationX = -1 * view.width * position
+            } else if (position <= 1) { // (0,1]
+                // Fade the page out.
+                view.alpha = 1 - position
+                view.translationX = -1 * view.width * position
+
+                // Scale the page down (between MIN_SCALE and 1)
+                val scaleFactor = (MIN_SCALE
+                        + (1 - MIN_SCALE) * (1 - Math.abs(position)))
+                view.scaleX = scaleFactor
+                view.scaleY = scaleFactor
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.alpha = 0f
+            }
+        }
     }
 
 }
